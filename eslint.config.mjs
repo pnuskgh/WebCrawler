ca850b8b9b793843de8bf72d8848d6ec2a016f45
Mountain Lover <pnuskgh@gmail.com>
2024-08-26 00:55:42 +0900

    Feature: .prettierignore 파일로 ESLinit의 ignores 생성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,49 +4,47 @@
  * @license OBCon License 1.0
  */
 
+import fs from "fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 // https://eslint.org/docs/latest/use/configure/language-options#predefined-global-variables
 import pluginJs from '@eslint/js';
 import globals from 'globals';                              //--- https://www.npmjs.com/package/globals
 import tseslint from 'typescript-eslint';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const ignorePath = path.resolve(__dirname, ".prettierignore");
+
+const ignores = fs.readFileSync(ignorePath, 'utf8').split('\n')
+                  .filter(line => ((line.startsWith('#') == false) && (line.trim() != '')))
+                  .map(ignore => ignore.replace('\r', ''));
+
 /**
  * @see https://www.npmjs.com/package/eslint
+ * @see https://eslint.org/docs/latest/use/configure/rules
  */
 const config = [
-    // {
-    //     // env: { browser: true, es2020: true },
-    //     // plugins: [],
-    //     // extends: [],
-    //     // rules: {},
-    //     // settings: {}
-    // },
     {
-        ignores: [                                        //--- https://eslint.org/docs/latest/use/configure/ignore
-            //--- 시스템 폴더
-            '**/.git/', '**/.github/', '**/.vscode', '**/node_modules/',
-            //--- 시스템 파일
-            '.env', '.gitignore', '.prettierignore', '**/.DS_Store', 'package.json',
-            '*-lock.json',
-            
-            //--- 사용자 폴더
-            'logs/',
-            //--- 사용자 파일
-            'prettier.config.mjs', 'eslint.config.mjs', '**/*.md', '**/*.html',
-
-            //--- 예외 처리
-            // "!설정"
-        ],
+        ignores: ignores
+        // env: { browser: true, es2020: true },
+        // plugins: [],
+        // extends: [],
+        // rules: {},
+        // settings: {}
     },
     {
-        files: [ '**/*.{js,mjs,cjs}' ],
+        files: [ '**/*.{js,mjs,cjs}' ]
     },
     {
         languageOptions: {
-            globals: globals.browser,
+            globals: globals.browser
         },
     },
     pluginJs.configs.recommended,                           //--- ESLint의 권장 설정
-    ...tseslint.configs.recommended,                        //--- TypeScript의 권장 설정
+    ...tseslint.configs.recommended                         //--- TypeScript의 권장 설정
 ];
 // console.log('globals.browser', globals.browser);
 // console.log('pluginJs.configs.recommended', pluginJs.configs.recommended);
