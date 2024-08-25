15b5164074e3729eceed1f8a0838c4d8fb96b071
Mountain Lover <pnuskgh@gmail.com>
2024-08-25 23:24:21 +0900

    feature: ESLint 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,9 +9,36 @@ import pluginJs from '@eslint/js';
 import globals from 'globals';                              //--- https://www.npmjs.com/package/globals
 import tseslint from 'typescript-eslint';
 
-export default [
+/**
+ * @see https://www.npmjs.com/package/eslint
+ */
+const config = [
+    // {
+    //     // env: { browser: true, es2020: true },
+    //     // plugins: [],
+    //     // extends: [],
+    //     // rules: {},
+    //     // settings: {}
+    // },
     {
-        files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+        ignores: [                                        //--- https://eslint.org/docs/latest/use/configure/ignore
+            //--- 시스템 폴더
+            '**/.git/', '**/.github/', '**/.vscode', '**/node_modules/',
+            //--- 시스템 파일
+            '.env', '.gitignore', '.prettierignore', '**/.DS_Store', 'package.json',
+            '*-lock.json',
+            
+            //--- 사용자 폴더
+            'logs/',
+            //--- 사용자 파일
+            'prettier.config.mjs', 'eslint.config.mjs', '**/*.md', '**/*.html',
+
+            //--- 예외 처리
+            // "!설정"
+        ],
+    },
+    {
+        files: [ '**/*.{js,mjs,cjs}' ],
     },
     {
         languageOptions: {
@@ -21,3 +48,8 @@ export default [
     pluginJs.configs.recommended,                           //--- ESLint의 권장 설정
     ...tseslint.configs.recommended,                        //--- TypeScript의 권장 설정
 ];
+// console.log('globals.browser', globals.browser);
+// console.log('pluginJs.configs.recommended', pluginJs.configs.recommended);
+// console.log('tseslint.configs.recommended', tseslint.configs.recommended);
+
+export default config;
